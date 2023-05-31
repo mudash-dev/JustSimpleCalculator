23f889256bce125a723213bdf0a5d1a2ff782057
mudash-dev <ashleymudambaji@gmail.com>
2023-05-31 08:25:48 +0300

    Dependency Injection using Koin

---
+++ app/src/main/java/com/example/simplecalculator/Modules.kt
@@ -0,0 +1,11 @@
+package com.example.simplecalculator
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+
+
+val viewModelModule:Module = module {
+     viewModel { CalculatorViewModel() }
+}
